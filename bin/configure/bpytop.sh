0d7b0e514b203fbe874bf21b78bafc67b51124b0
Stephen Moloney <stephen@stephenmoloney.com>
2021-05-24 13:18:25 +0000

    Added: bpytop installation and configuration
    ***What does this change do?***
    
    - Added: bpytop installation and configuration
    
    ***Why is this change needed?***
    
    - To have a bpytop installation and configuration

---
+++ bin/configure/bpytop.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2125
+set -eo pipefail
+
+# ******* Importing utils.sh as a source of common shell functions *******
+GITHUB_URL=https://raw.githubusercontent.com/stephenmoloney/localbox/master
+UTILS_PATH="$(dirname "${BASH_SOURCE[0]}")/../utils.sh"
+if [[ -e "${UTILS_PATH}" ]]; then
+    source "${UTILS_PATH}"
+else
+    if [[ -z "$(command -v curl)" ]]; then
+        sudo apt update -y -qq
+        sudo apt install -y -qq curl
+    fi
+    echo "Falling back to remote script ${GITHUB_URL}/bin/utils.sh"
+    if curl -sIf -o /dev/null ${GITHUB_URL}/bin/utils.sh; then
+        source <(curl -s "${GITHUB_URL}/bin/utils.sh")
+    else
+        echo "${GITHUB_URL}/bin/utils.sh does not exist" >/dev/stderr
+        return 1
+    fi
+fi
+# ************************************************************************
+PROJECT_ROOT="$(project_root)"
+
+function setup_bpytop_dotfiles() {
+    if [[ ! -d "${HOME}/.config/bpytop" ]]; then
+        mkdir -p "${HOME}/.config/bpytop"
+    fi
+    cp \
+        "${PROJECT_ROOT}/config/dotfiles/bpytop/bpytop.ini" \
+        "${HOME}/.config/bpytop/bpytop.cfg"
+}
+
+function setup_bpytop() {
+    if [[ -e "${HOME}/.local/bin/bpytop" ]] &&
+        [[ -z "$(grep "${HOME}/.local/bin" <<<"${PATH}" 2>/dev/null || true)" ]]; then
+        export PATH="${PATH}:${HOME}/.local/bin"
+    fi
+}
