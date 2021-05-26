d4a864e8855a902b951387e1c5cf1fe25e9d0ab3
Stephen Moloney <stephen@stephenmoloney.com>
2021-05-26 19:43:06 +0000

    Interim commit - installation broken

---
+++ bin/install/black.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+set -eu
+set -o pipefail
+set -o errtrace
+
+BLACK_VERSION_FALLBACK=0.16.2.1
+
+# ******* Importing utils.sh as a source of common shell functions *******
+GITHUB_URL=https://raw.githubusercontent.com/stephenmoloney/localbox/master
+UTILS_PATH="$(dirname "${BASH_SOURCE[0]}")/../utils.sh"
+if [[ -e "${UTILS_PATH}" ]]; then
+    source "${UTILS_PATH}"
+else
+    if [[ -z "$(command -v curl)" ]]; then
+        sudo apt update -y -qq
+        sudo apt install -y -qq curl
+    fi
+    echo "Falling back to remote script ${GITHUB_URL}/bin/utils.sh"
+    if curl -sIf -o /dev/null ${GITHUB_URL}/bin/utils.sh; then
+        source <(curl -s "${GITHUB_URL}/bin/utils.sh")
+    else
+        echo "${GITHUB_URL}/bin/utils.sh does not exist" >/dev/stderr
+        return 1
+    fi
+fi
+# ************************************************************************
+
+function get_current_version() {
+    black --version
+}
+
+function install_black() {
+    local version="${1}"
+
+    maybe_install_apt_pkg "python3-pip" "*"
+
+    if [[ -z "$(get_current_version 2>/dev/null || true)" ]] ||
+        [[ "$(get_current_version 2>/dev/null || true)" != "${version}" ]]; then
+        pip3 install black=="${version}"
+    else
+        echo "black version ${version} is already installed"
+        echo "Skipping installation"
+    fi
+
+    if [[ -z "$(grep "${HOME}/.local/bin" <<<"${PATH}" 2>/dev/null || true)" ]]; then
+        export PATH="${PATH}:${HOME}/.local/bin"
+    fi
+
+    black --version
+}
+
+function main() {
+    local version="${1:-$BLACK_VERSION_FALLBACK}"
+
+    install_black "${version}"
+}
+
+if [[ "$0" == "${BASH_SOURCE[0]}" ]]; then
+    main "${@}"
+fi
