75d775aa2b1983f270011b794f870e27dbc7ad95
Stephen Moloney <stephen@stephenmoloney.com>
2021-05-27 13:57:06 +0200

    Added: pgcli tool (#20)
    * Added: pgcli tool
    
    ***What does this change do?***
    
    - Added: pgcli tool
    
    ***Why is this change needed?***
    
    - tool for postgres databases and also installs postgresql client
    
    * Install postgresql client prerequisites
    
    * Fixed - broken installation of pgcli
    
    * Added: missing libpq-dev dep

---
+++ bin/install/pgcli.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+set -eu
+set -o pipefail
+set -o errtrace
+
+PGCLI_VERSION_FALLBACK=3.1.0
+POSTGRESQL_CLIENT_VERSION_FALLBACK=13
+
+# ******* Importing utils.sh as a source of common shell functions *******
+GITHUB_URL=https://raw.githubusercontent.com/stephenmoloney/localbox/master
+UTILS_PATH="$(dirname "${BASH_SOURCE[0]}")/../utils.sh"
+if [[ -e "${UTILS_PATH}" ]]; then
+    source "${UTILS_PATH}"
+else
+    if [[ -z "$(command -v curl)" ]]; then
+        sudo apt update -y -qq
+        sudo apt install -y -qq curl
+    fi
+    echo "Falling back to remote script ${GITHUB_URL}/bin/utils.sh"
+    if curl -sIf -o /dev/null ${GITHUB_URL}/bin/utils.sh; then
+        source <(curl -s "${GITHUB_URL}/bin/utils.sh")
+    else
+        echo "${GITHUB_URL}/bin/utils.sh does not exist" >/dev/stderr
+        return 1
+    fi
+fi
+# ************************************************************************
+
+function get_current_version() {
+    pgcli --version | cut -d' ' -f2
+}
+
+function postgresql_client_prerequisites() {
+    maybe_install_apt_pkg "wget" "*"
+
+    wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc |
+        sudo apt-key add -
+
+    if [[ ! -e /etc/apt/sources.list.d/pgdg.list ]]; then
+        echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" |
+            sudo tee /etc/apt/sources.list.d/pgdg.list
+    fi
+
+    sudo apt update -y -qq
+    maybe_install_apt_pkg "libpq-dev" "*"
+}
+
+function install_pgcli() {
+    local pgcli_version="${1}"
+    local postgresql_client_version="${2}"
+
+    maybe_install_apt_pkg "python3-dev" "*"
+    maybe_install_apt_pkg "python3-pip" "*"
+    maybe_install_apt_pkg "lsb-release" "*"
+    postgresql_client_prerequisites
+    maybe_install_apt_pkg "postgresql-client-${postgresql_client_version}" "*"
+
+    if [[ -z "$(get_current_version 2>/dev/null || true)" ]] ||
+        [[ "$(get_current_version 2>/dev/null || true)" != "${pgcli_version}" ]]; then
+        pip3 install pgcli=="${pgcli_version}"
+    else
+        echo "pgcli version ${pgcli_version} is already installed"
+        echo "Skipping installation"
+    fi
+
+    if [[ -z "$(grep "${HOME}/.local/bin" <<<"${PATH}" 2>/dev/null || true)" ]]; then
+        export PATH="${PATH}:${HOME}/.local/bin"
+    fi
+
+    pgcli --version
+}
+
+function main() {
+    local pgcli_version="${1:-$PGCLI_VERSION_FALLBACK}"
+    local postgresql_client_version="${2:-$POSTGRESQL_CLIENT_VERSION_FALLBACK}"
+
+    install_pgcli "${pgcli_version}" "${postgresql_client_version}"
+}
+
+if [[ "$0" == "${BASH_SOURCE[0]}" ]]; then
+    main "${@}"
+fi
