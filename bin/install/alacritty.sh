3320a2041aa92c3ac390aa94e1eef7002f246d37
Stephen Moloney <stephen@stephenmoloney.com>
2021-05-30 08:46:21 +0000

    Added: alacritty version bumped to 0.8.0

--- bin/install/alacritty.sh
+++ bin/install/alacritty.sh
@@ -3,7 +3,7 @@ set -eu
 set -o pipefail
 set -o errtrace
 
-ALACRITTY_VERSION_FALLBACK=0.7.2
+ALACRITTY_VERSION_FALLBACK=0.8.0
 
 # ******* Importing utils.sh as a source of common shell functions *******
 GITHUB_URL=https://raw.githubusercontent.com/stephenmoloney/localbox/master
