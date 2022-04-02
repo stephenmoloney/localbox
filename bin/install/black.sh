8fd8f14a2fa3d9efe831b7c4a043d99c1464903e
Stephen Moloney <stephen@stephenmoloney.com>
2022-04-02 19:43:46 +0000

    Fixed: ruby version 3.0.2 not installable
    ***What does this change do?***
    
    - drop back to ruby 3.0.1
    - black version installation increased
    - missing source on kafka_pkgs added
    
    ***Why is this change needed?***
    
    - ruby version 3.0.2 not installable
    - previous version of black installed failing due to click dep.

--- bin/install/black.sh
+++ bin/install/black.sh
@@ -3,7 +3,7 @@ set -eu
 set -o pipefail
 set -o errtrace
 
-BLACK_VERSION_FALLBACK=22.1.0
+BLACK_VERSION_FALLBACK=22.3.0
 
 # ******* Importing utils.sh as a source of common shell functions *******
 GITHUB_URL=https://raw.githubusercontent.com/stephenmoloney/localbox/master
