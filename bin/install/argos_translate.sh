78b744d58a1f75d122e20ba33db91c2d2de8b77a
Stephen Moloney <stephen@stephenmoloney.com>
2022-04-03 11:51:18 +0200

    Added: argos translate | offline translation
    ***What does this change do?***
    
    - Install argos translate gui app
    
    ***Why is this change needed?***
    
    - Keep argos translate gui app installed

---
+++ bin/install/argos_translate.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+set -eu
+set -o pipefail
+set -o errtrace
+
+ARGOS_TRANSLATE_GUI_VERSION_FALLBACK=1.6.3
+
+# ******* Importing utils.sh as a source of common shell functions *******
+GITHUB_URL=https://raw.githubusercontent.com/stephenmoloney/localbox/master
+UTILS_PATH="$(dirname "${BASH_SOURCE[0]}")/../utils.sh"
+if [[ -e "${UTILS_PATH}" ]]; then
+    source "${UTILS_PATH}"
+else
+    if [[ -z "$(command -v curl)" ]]; then
+        sudo apt update -y -qq
+        sudo apt install -y -qq curl
+    fi
+    echo "Falling back to remote script ${GITHUB_URL}/bin/utils.sh"
+    if curl -sIf -o /dev/null ${GITHUB_URL}/bin/utils.sh; then
+        source <(curl -s "${GITHUB_URL}/bin/utils.sh")
+    else
+        echo "${GITHUB_URL}/bin/utils.sh does not exist" >/dev/stderr
+        return 1
+    fi
+fi
+# ************************************************************************
+
+function install_argos_translate_gui() {
+    local version="${1}"
+
+    maybe_install_apt_pkg "python3-pip" "*"
+    pip3 install argostranslategui=="${version}"
+
+    if [[ -z "$(grep "${HOME}/.local/bin" <<<"${PATH}" 2>/dev/null || true)" ]]; then
+        export PATH="${PATH}:${HOME}/.local/bin"
+    fi
+}
+
+function main() {
+    local version="${1:-$ARGOS_TRANSLATE_GUI_VERSION_FALLBACK}"
+
+    install_argos_translate_gui "${version}"
+}
+
+if [[ "$0" == "${BASH_SOURCE[0]}" ]]; then
+    main "${@}"
+fi
