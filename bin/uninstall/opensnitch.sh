3b1d53bde6c95e6a47d1f329215ca0458fe8cb05
Stephen Moloney <stephen@stephenmoloney.com>
2022-07-31 21:45:14 +0000

    Fixed: missing websocat version on downloads

---
+++ bin/uninstall/opensnitch.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+set -eu
+set -o pipefail
+set -o errtrace
+
+# ******* Importing utils.sh as a source of common shell functions *******
+GITHUB_URL=https://raw.githubusercontent.com/stephenmoloney/localbox/master
+UTILS_PATH="$(dirname "${BASH_SOURCE[0]}")/../utils.sh"
+if [[ -e "${UTILS_PATH}" ]]; then
+    source "${UTILS_PATH}"
+else
+    if [[ -z "$(command -v curl)" ]]; then
+        sudo apt update -y -qq
+        sudo apt install -y -qq curl
+    fi
+    echo "Falling back to remote script ${GITHUB_URL}/bin/utils.sh"
+    if curl -sIf -o /dev/null ${GITHUB_URL}/bin/utils.sh; then
+        source <(curl -s "${GITHUB_URL}/bin/utils.sh")
+    else
+        echo "${GITHUB_URL}/bin/utils.sh does not exist" >/dev/stderr
+        return 1
+    fi
+fi
+# ************************************************************************
+
+function uninstall_opensnitch() {
+    maybe_uninstall_apt_pkg "opensnitch"
+    maybe_uninstall_apt_pkg "python3-opensnitch-ui"
+}
+
+function main() {
+    uninstall_opensnitch
+}
+
+main
