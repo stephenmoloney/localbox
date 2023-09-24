5cfa9f258b6ed8a52fae75065fb9d263a370c2f6
Stephen Moloney <stephen@stephenmoloney.com>
2023-09-24 13:00:12 +0000

    squash

--- bin/main.sh
+++ bin/main.sh
@@ -83,6 +83,7 @@ function install() {
     exec_with_retries "${PROJECT_ROOT}/bin/install/krew.sh" 0 2 "${KREW_VERSION:-}"
     "${PROJECT_ROOT}/bin/install/pgcli.sh" "${PGCLI_VERSION:-}" "${POSTGRESQL_CLIENT_VERSION:-}"
     exec_with_retries "${PROJECT_ROOT}/bin/install/nerd_fonts.sh" 0 2 "${NERDFONTS_VERSION:-}"
+    exec_with_retries "${PROJECT_ROOT}/bin/install/nerdctl.sh" 0 2 "${NERDCTL_VERSION:-}"
     exec_with_retries "${PROJECT_ROOT}/bin/install/pencil.sh" 0 2 "${PENCIL_VERSION:-}"
     exec_with_retries "${PROJECT_ROOT}/bin/install/pipx.sh" 0 2 "${PIPX_VERSION:-}"
     exec_with_retries "${PROJECT_ROOT}/bin/install/rust_pkgs.sh" 0 2
