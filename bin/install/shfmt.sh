541bdc6c19ea7b6d6025467e3a3527e8f42e9ed2
Stephen Moloney <stephen@stephenmoloney.com>
2024-03-15 19:50:30 +0000

    Debugging

--- bin/install/shfmt.sh
+++ bin/install/shfmt.sh
@@ -3,7 +3,7 @@ set -eu
 set -o pipefail
 set -o errtrace
 
-SHFMT_VERSION_FALLBACK=3.7.0
+SHFMT_VERSION_FALLBACK=3.8.0
 
 # ******* Importing utils.sh as a source of common shell functions *******
 GITHUB_URL=https://raw.githubusercontent.com/stephenmoloney/localbox/master
